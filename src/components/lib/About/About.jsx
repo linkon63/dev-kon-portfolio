49dd18df54436c2b74e378f60c63c6f301f39024
Abraham Linkon <37959190+linkon63@users.noreply.github.com>
2022-05-15 00:03:50 +0600

    added about jsx

--- src/components/lib/About/About.jsx
+++ src/components/lib/About/About.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import Layout from '../Layout/Layout';
+import AboutComponent from './AboutComponent';
 
 const About = () => {
     return (
-        <div>
-            About
-        </div>
+        <Layout key={Math.random()} props={<AboutComponent />} />
     );
 };
 
